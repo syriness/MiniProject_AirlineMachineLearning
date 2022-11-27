75629cd3d42987374934489933c176cd68836f3c
syriness <117613789+syriness@users.noreply.github.com>
2022-11-28 00:00:19 +0900

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import kmeans
 import prepro
-import eval
+import logic
 
 
 # streamlit 앱 제목
@@ -29,24 +29,8 @@ kmeans.kmeans_clustering(airline, airline2)
     
 X, y, airline_test, airline_test_X = prepro.preprocess(airline, airline2)
     
-# 결정트리
-from sklearn.tree import DecisionTreeClassifier
-
-st.header("결정트리 학습 결과")
-
-dt = DecisionTreeClassifier(max_depth=20, min_samples_leaf=5, min_samples_split=100, random_state=100)
-dt.fit(X, y)
-pred_dt = dt.predict(airline_test_X)
-eval.evaluation(airline_test, pred_dt)
-# accuracy: 0.9324, precision: 0.9373, recall: 0.9066, fl score: 0.9217, roc_auc_score: 0.9296
-
+st.write("")
 st.write("")
 st.write("")
 
-st.subheader("결정트리 학습 시 중요한 변수들")
-
-dt.imp = pd.DataFrame({"satisfaction":X.columns, "values":dt.feature_importances_})
-
-fig_dt = plt.figure(figsize=(20, 10))
-sns.barplot(x="values", y= "satisfaction", data=dt.imp.sort_values(by="values", ascending=False))
-st.pyplot(fig_dt)
+logic.logic_reg(X, y, airline_test, airline_test_X)
