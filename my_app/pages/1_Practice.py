fa819fe4218bc3dee26d7c818eebc44d469e4953
syriness <117613789+syriness@users.noreply.github.com>
2022-11-29 13:07:10 +0900

    Update 1_Practice.py

--- my_app/pages/1_Practice.py
+++ my_app/pages/1_Practice.py
@@ -17,37 +17,37 @@ st.subheader("6가지의 머신러닝 모델을 활용하여 변수들을 바꾸
 # 첫 번째 행
 r1_col1, r1_col2, r1_col3 = st.columns(3)
 
-Inflight_wifi_service = r1_col1.slider("Inflight wifi service", 0, 5)
+Inflight_wifi_service = r1_col1.slider("Inflight wifi service", 1, 5)
 
-Departure_Arrival_time_convenient = r1_col2.slider("Departure/Arrival time convenient", 0, 5)
+Departure_Arrival_time_convenient = r1_col2.slider("Departure/Arrival time convenient", 1, 5)
 
-Ease_of_Online_booking = r1_col3.slider("Ease of Online booking", 0,5)
+Ease_of_Online_booking = r1_col3.slider("Ease of Online booking", 1,5)
 
 
 # 두번째 행
 r2_col1, r2_col2, r2_col3 = st.columns(3)
 
-Gate_location = r2_col1.slider("Gate location",0,5 )
-Food_and_drink = r2_col2.slider("Food and drink", 0,5)
-Online_boarding = r2_col3.slider("Online boarding", 0,5)
+Gate_location = r2_col1.slider("Gate location",1,5 )
+Food_and_drink = r2_col2.slider("Food and drink", 1,5)
+Online_boarding = r2_col3.slider("Online boarding", 1,5)
 
 # 세번째 행
 r3_col1, r3_col2, r3_col3 = st.columns(3)
-Leg_room_service=r3_col1.slider("Leg room service ",0,5)
-Baggage_handling =r3_col2.slider("Baggage handling",0,5)
-On_board_service =r3_col3.slider("On-board service",0,5)
+Leg_room_service=r3_col1.slider("Leg room service ",1,5)
+Baggage_handling =r3_col2.slider("Baggage handling",1,5)
+On_board_service =r3_col3.slider("On-board service",1,5)
 
 
 # 네 번째 행
 r4_col1, r4_col2, r4_col3 = st.columns(3)
-Seat_comfort =r4_col1.slider("Seat comfort ",0,5)
-Inflight_entertainment =r4_col2.slider("Inflight entertainment",0,5)
-Check_in_service =r4_col3.slider("Check-in service",0,5)
+Seat_comfort =r4_col1.slider("Seat comfort ",1,5)
+Inflight_entertainment =r4_col2.slider("Inflight entertainment",1,5)
+Check_in_service =r4_col3.slider("Check-in service",1,5)
 
 # 다섯번째 행
 r5_col1, r5_col2 = st.columns(2)
-Inflight_service =r5_col1.slider("Inflight service ",0,5)
-Cleanliness =r5_col2.slider("Cleanliness",0,5)
+Inflight_service =r5_col1.slider("Inflight service ",1,5)
+Cleanliness =r5_col2.slider("Cleanliness",1,5)
 
 # 여섯번째 행
 r6_col1, r6_col2 = st.columns(2)
