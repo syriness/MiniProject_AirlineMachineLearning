cf83b2464c2eb1362a8db4a0ab34547df1cacf8f
Yoo-Ha-young <116700717+Yoo-Ha-young@users.noreply.github.com>
2022-11-29 11:03:44 +0900

    Update and rename streamlit_app.py to Airline_passenger_satisfaction.py

---
+++ my_app/Airline_passenger_satisfaction.py
@@ -0,0 +1,20 @@
+# Contents of ~/my_app/Airline_passenger_satisfaction.py
+import streamlit as st
+
+def main_page():
+    st.markdown("# Airline passenger satisfaction")
+    st.sidebar.markdown("# Airline passenger satisfaction")
+
+def page():
+    st.markdown("# Practice")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+
+
+page_names_to_funcs = {
+    "Practice": Practice,
+     "Page": page,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
